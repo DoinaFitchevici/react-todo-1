34bed6e3c9a797936f98e32b379d9c00b91e056e
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-02 15:03:59 -0400

    Change link style on hover

--- src/App.js
+++ src/App.js
@@ -18,12 +18,14 @@ function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-gray-800 my-4">Todo List</h1>
-      <ul class="list-disc ml-10" role="list">
+      <ul class="list-disc ml-10">
         {todoList.map((todo) => (
           <li key={todo.id}>
             <span>
               <a href={todo.url} target="_blank" rel="noopener noreferrer">
-                <span className="text-blue-500"> {todo.title}</span>
+                <span className="text-blue-500 hover:text-black hover:underline">
+                  {todo.title}
+                </span>
               </a>
             </span>
           </li>
