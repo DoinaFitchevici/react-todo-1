a331d902ec079e7e94607ea3457410a0ca9e0237
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-07 17:51:56 -0400

    Create Todo List component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from "react";
-
-const todoList = [
-  {
-    id: 1,
-    title: "Learn React",
-    url: "https://reactjs.org/",
-  },
-  {
-    id: 2,
-    title: "Learn Redux",
-    url: "https://redux.js.org/",
-  },
-  { id: 3, title: "Learn React Native", url: "https://reactnative.dev/" },
-];
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div>
       <h1 className="text-4xl font-bold text-gray-800 my-4">Todo List</h1>
-      <ul class="list-disc ml-10">
-        {todoList.map((todo) => (
-          <li key={todo.id}>
-            <a
-              href={todo.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:text-black hover:underline"
-            >
-              {todo.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const todoList = [
+  {
+    id: 1,
+    title: "Learn React",
+    url: "https://reactjs.org/",
+  },
+  {
+    id: 2,
+    title: "Learn Redux",
+    url: "https://redux.js.org/",
+  },
+  { id: 3, title: "Learn React Native", url: "https://reactnative.dev/" },
+];
+
+function TodoList() {
+  return (
+    <ul class="list-disc ml-10">
+      {todoList.map((todo) => (
+        <li key={todo.id}>
+          <a
+            href={todo.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:text-black hover:underline"
+          >
+            {todo.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
