bedefeca9ab5cfea2aeef4088945662970cbd8e9
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-17 06:44:16 -0400

    Handle undefined cases for props.todo

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,4 +1,9 @@
 function TodoListItem(props) {
+  // handle the case when props.todo is undefined or null to prevent potential runtime errors.
+  if (!props.todo) {
+    return null;
+  }
+  // Destructure todo prop to get title and url
   const { title, url } = props.todo;
   return (
     <li>
