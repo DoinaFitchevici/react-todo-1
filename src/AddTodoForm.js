0d65a7bbaba401e714cdefae7e4b887ecef40151
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-17 08:09:46 -0400

    Store, New Todo, in React State

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,9 +1,11 @@
-function AddTodoForm() {
+function AddTodoForm(props) {
   function handleAddTodo(event) {
     event.preventDefault();
     // Retrieve the value of the title element from the event target
     const todoTitle = event.target.elements.title.value;
     console.log(todoTitle);
+    // Invoke the onAddTodo callback prop and pass todoTitle as an argument
+    props.onAddTodo(todoTitle);
     // Reset the form so the text input value is cleared
     event.target.reset();
   }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
