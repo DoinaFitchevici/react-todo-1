67cac6cdee9a78d2ef5f9ae248919d6b2bb83775
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-17 07:36:07 -0400

    Handle "Add Todo Form" Submit

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,16 @@
-import React from "react";
-
 function AddTodoForm() {
+  function handleAddTodo(event) {
+    event.preventDefault();
+    // Retrieve the value of the title element from the event target
+    const todoTitle = event.target.elements.title.value;
+    console.log(todoTitle);
+    // Reset the form so the text input value is cleared
+    event.target.reset();
+  }
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle"></input>
+      <input type="text" id="todoTitle" name="title"></input>
       <button type="submit">Add</button>
     </form>
   );
