4cba2eb25481a493a4da934c47a805c0c799b876
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-30 00:15:10 -0400

    Move Todo List into State

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,9 @@
 import TodoListItem from "./TodoListItem";
 
-const todoList = [
-  {
-    id: 1,
-    title: "Learn React",
-    url: "https://reactjs.org/",
-  },
-  {
-    id: 2,
-    title: "Learn Redux",
-    url: "https://redux.js.org/",
-  },
-  { id: 3, title: "Learn React Native", url: "https://reactnative.dev/" },
-];
-
-function TodoList() {
+function TodoList(props) {
   return (
     <ul>
-      {todoList.map((todo) => (
+      {props.todoList.map((todo) => (
         <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
