826947781ffce1d734153406c61b03480afd0684
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-11-02 19:50:13 -0400

    Remove early return in TodoListItem

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,8 +1,4 @@
 function TodoListItem({ todo }) {
-  // handle the case when props.todo is undefined or null to prevent potential runtime errors.
-  if (!todo) {
-    return null;
-  }
   // Destructure todo prop to get title and url
   const { title, url } = todo;
   return (
