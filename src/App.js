1082d5a03d2ded6be58aeb7ca23898d10629d298
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-12-07 01:28:54 -0500

    Fetch todo list from Airtable API instead of local storage

--- src/App.js
+++ src/App.js
@@ -7,19 +7,39 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [completionMessage, setCompletionMessage] = useState("");
 
-  useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
-          },
-        });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      const todos = data.records.map((record) => ({
+        title: record.fields.title,
+        id: record.id,
+      }));
+      setTodoList(todos);
       setIsLoading(false);
-    });
+    } catch (error) {
+      console.log(error);
+    }
+    return {
+      data: {
+        todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+      },
+    };
+    // console.log(data);
+  };
+  useEffect(() => {
+    fetchData();
   }, []);
 
   useEffect(() => {
