1387f66a9e8f27e1d957f6c9ca6021d1f34f564f
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-12-17 04:30:32 -0500

    Update Airtable API calls to use axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,26 +18,23 @@ function App() {
   const URL = `${API_BASE_URL}${AIRTABLE_BASE_ID}/${TABLE_NAME}`;
 
   const fetchData = async () => {
-    const options = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
-      },
-    };
-
     const fetchURL = `${URL}${SORT_BY_LAST_MODIFIED_TIME}`;
 
     try {
-      const response = await fetch(fetchURL, options);
-      if (!response.ok) {
-        throw new Error(`Error fetching data: ${response.status}`);
-      }
-      const { records } = await response.json();
-      const todos = records.map(({ fields: { title, completed }, id }) => ({
-        id,
-        title,
-        completed,
-      }));
+      const response = await axios.get(fetchURL, {
+        headers: {
+          Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
+        },
+      });
+
+      const todos = response.data.records.map(
+        ({ fields: { title, completed }, id }) => ({
+          id,
+          title,
+          completed,
+        })
+      );
+
       return todos;
     } catch (error) {
       throw new Error(`Error fetching data: ${error.message}`);
@@ -69,22 +67,14 @@ function App() {
         },
       };
       const updateURL = `${URL}/${todo.id}${SORT_BY_LAST_MODIFIED_TIME}`;
-      const response = await fetch(updateURL, {
-        method: "PATCH",
+      const response = await axios.patch(updateURL, airtableData, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
         },
-        body: JSON.stringify(airtableData),
       });
 
-      if (!response.ok) {
-        const message = `Error updating todo: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const dataResponse = await response.json();
-      return dataResponse;
+      return response.data;
     } catch (error) {
       throw new Error(`Error updating todo: ${error.message}`);
     }
@@ -92,55 +82,39 @@ function App() {
 
   const deleteTodo = async (id) => {
     try {
-      const response = await fetch(`${URL}/${id}`, {
-        method: "DELETE",
+      const response = await axios.delete(`${URL}/${id}`, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
         },
       });
 
-      if (!response.ok) {
-        const message = `Error deleting todo: ${response.status}`;
-        throw new Error(message);
-      }
-
-      const dataResponse = await response.json();
-      return dataResponse;
+      return response.data;
     } catch (error) {
       throw new Error(`Error deleting todo: ${error.message}`);
     }
   };
 
   const addTodo = async (newTodo) => {
-    // Make a POST request to add a new todo to Airtable
     const options = {
-      method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
       },
-      body: JSON.stringify({
-        fields: {
-          title: newTodo.title,
-        },
-      }),
     };
 
     try {
-      const response = await fetch(URL, options);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
+      const response = await axios.post(
+        URL,
+        { fields: { title: newTodo.title } },
+        options
+      );
 
-      const responseData = await response.json();
-
-      // Update the todo list with the new todo received from the API response
       setTodoList((prevTodoList) => [
         ...prevTodoList,
         {
-          id: responseData.id,
-          title: responseData.fields.title,
+          id: response.data.id,
+          title: response.data.fields.title,
           completed: false,
         },
       ]);
@@ -151,8 +125,8 @@ function App() {
     }
   };
 
-  const removeTodo = (id) => {
-    deleteTodo(id);
+  const removeTodo = async (id) => {
+    await deleteTodo(id);
     setTodoList(todoList.filter((todo) => todo.id !== id));
   };
 
@@ -160,11 +134,12 @@ function App() {
     setTodoList(newTodoList);
   };
 
-  const onToggleComplete = (id, completed) => {
+  const onToggleComplete = async (id, completed) => {
     const updatedTodoList = todoList.map((todo) =>
       todo.id === id ? { ...todo, completed } : todo
     );
-    updateTodo(updatedTodoList.find((itemTodo) => itemTodo.id === id));
+
+    await updateTodo(updatedTodoList.find((itemTodo) => itemTodo.id === id));
     setTodoList(updatedTodoList);
   };
 
