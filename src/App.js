f42dbef1db222a00a23e484cbdabec7acc269d3a
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-12-17 03:15:42 -0500

    The URL parameters are extracted to constants to avoid duplication.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import { useState, useEffect, useCallback } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { fetchData } from "api";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [completionMessage, setCompletionMessage] = useState("");
+
+  const API_BASE_URL = "https://api.airtable.com/v0/";
+  const AIRTABLE_BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
+  const TABLE_NAME = process.env.REACT_APP_TABLE_NAME;
+  const AIRTABLE_API_TOKEN = process.env.REACT_APP_AIRTABLE_API_TOKEN;
+  const sortByLastModifiedTime =
+    "?sort[0][field]=completed&sort[0][direction]=asc&sort[1][field]=lastModifiedTime&sort[1][direction]=asc";
+  const URL = `${API_BASE_URL}${AIRTABLE_BASE_ID}/${TABLE_NAME}`;
+
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
+      },
+    };
+
+    const url = `${URL}${sortByLastModifiedTime}`;
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const { records } = await response.json();
+      const todos = records.map(({ fields: { title, completed }, id }) => ({
+        id,
+        title,
+        completed,
+      }));
+      return todos;
+    } catch (error) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    }
+  };
 
   const memoizedFetchData = useCallback(async () => {
     try {
@@ -34,12 +68,12 @@ function App() {
           completed: todo.completed,
         },
       };
-      const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${todo.id}`;
+      const url = `${URL}/${todo.id}${sortByLastModifiedTime}`;
       const response = await fetch(url, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+          Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
         },
         body: JSON.stringify(airtableData),
       });
@@ -60,12 +94,12 @@ function App() {
 
   const deleteTodo = async (id) => {
     try {
-      const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${id}`;
+      const url = `${URL}/${id}`;
       const response = await fetch(url, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+          Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
         },
       });
 
@@ -85,12 +119,12 @@ function App() {
 
   const addTodo = async (newTodo) => {
     // Make a POST request to add a new todo to Airtable
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    const url = `${URL}`;
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+        Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
       },
       body: JSON.stringify({
         fields: {
